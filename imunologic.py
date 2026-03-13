9e09b3bbb2e35ba41e964076dcf8a80bd72ee3b5
vilsongreinert-tech <vilsongreinert@gmail.com>
2026-03-13 19:33:26 -0300

    Update imunologic.py

--- imunologic.py
+++ imunologic.py
@@ -3,7 +3,7 @@ import streamlit as st
 from openai import OpenAI
 from pypdf import PdfReader
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
